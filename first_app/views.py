432115e22f044e5ccd585819d50973f601d2bded
riyadhswe <riyadhswe@gmail.com>
2021-10-26 09:14:15 +0600

    Update Data - Part 3

--- first_app/views.py
+++ first_app/views.py
@@ -7,14 +7,14 @@ from first_app import forms
 # Create your views here.
 def index(request):
     musician_list = Musician.objects.order_by('first_name')
-    diction = {'Title': 'This is Home Page!','musician_list': musician_list }
+    diction = {'Title': 'This is Home Page!', 'musician_list': musician_list}
     return render(request, 'first_app/index.html', context=diction)
 
 
-def album_list(request,artist_id):
+def album_list(request, artist_id):
     artist_info = Musician.objects.get(pk=artist_id)
-    album_list = Album.objects.filter(artist=artist_id).order_by('name','release_date')
-    diction = {'Title': 'List of Albums','artist_info':artist_info,'album_list':album_list}
+    album_list = Album.objects.filter(artist=artist_id).order_by('name', 'release_date')
+    diction = {'Title': 'List of Albums', 'artist_info': artist_info, 'album_list': album_list}
     return render(request, 'first_app/album_list.html', context=diction)
 
 
@@ -33,23 +33,35 @@ def musician_form(request):
 
 def album_form(request):
     form = forms.AlbumForm()
-    if request.method =='POST':
+    if request.method == 'POST':
         form = forms.AlbumForm(request.POST)
 
         if form.is_valid():
             form.save(commit=True)
             return index(request)
 
-    diction = {'Title': 'Add Album','album_form':form}
+    diction = {'Title': 'Add Album', 'album_form': form}
     return render(request, 'first_app/album_form.html', context=diction)
 
-def edit_artist(request,artist_id):
+
+def edit_artist(request, artist_id):
     artist_info = Musician.objects.get(pk=artist_id)
     form = forms.MusicianForm(instance=artist_info)
-    diction = {'edit_form':form}
+    diction = {'edit_form': form}
     if request.method == 'POST':
-        form = forms.MusicianForm(request.POST,instance=artist_info)
+        form = forms.MusicianForm(request.POST, instance=artist_info)
         if form.is_valid():
             form.save(commit=True)
-            return album_list(request,artist_id)
+            return album_list(request, artist_id)
     return render(request, 'first_app/edit_artist.html', context=diction)
+
+
+def edit_album(request, album_id):
+    album_info = Album.objects.get(pk=3)
+    form = forms.AlbumForm(instance=album_info)
+    if request.method == 'POST':
+        form = forms.AlbumForm(request.POST, instance=album_info)
+        if form.is_valid():
+            form.save(commit=True)
+    diction = {'edit_form': form}
+    return render(request, 'first_app/edit_album.html', context=diction)
